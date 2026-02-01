e1d493a72f7e4ef33797ed1feb3c8eaae24ffe41
Afeef K K <45afeef@gmail.com>
2026-02-01 14:24:52 +0530

    run build script

--- dart_sdk/lib/src/model/cab_create.g.dart
+++ dart_sdk/lib/src/model/cab_create.g.dart
@@ -11,11 +11,34 @@ class _$CabCreate extends CabCreate {
   final String vehicleType;
   @override
   final String vehicleNumber;
+  @override
+  final num minimumRate;
+  @override
+  final num kmForMinimumRate;
+  @override
+  final num perKmRate;
+  @override
+  final int capacity;
+  @override
+  final String name;
+  @override
+  final String companyModel;
+  @override
+  final String color;
 
   factory _$CabCreate([void Function(CabCreateBuilder)? updates]) =>
       (CabCreateBuilder()..update(updates))._build();
 
-  _$CabCreate._({required this.vehicleType, required this.vehicleNumber})
+  _$CabCreate._(
+      {required this.vehicleType,
+      required this.vehicleNumber,
+      required this.minimumRate,
+      required this.kmForMinimumRate,
+      required this.perKmRate,
+      required this.capacity,
+      required this.name,
+      required this.companyModel,
+      required this.color})
       : super._();
   @override
   CabCreate rebuild(void Function(CabCreateBuilder) updates) =>
@@ -29,7 +52,14 @@ class _$CabCreate extends CabCreate {
     if (identical(other, this)) return true;
     return other is CabCreate &&
         vehicleType == other.vehicleType &&
-        vehicleNumber == other.vehicleNumber;
+        vehicleNumber == other.vehicleNumber &&
+        minimumRate == other.minimumRate &&
+        kmForMinimumRate == other.kmForMinimumRate &&
+        perKmRate == other.perKmRate &&
+        capacity == other.capacity &&
+        name == other.name &&
+        companyModel == other.companyModel &&
+        color == other.color;
   }
 
   @override
@@ -37,6 +67,13 @@ class _$CabCreate extends CabCreate {
     var _$hash = 0;
     _$hash = $jc(_$hash, vehicleType.hashCode);
     _$hash = $jc(_$hash, vehicleNumber.hashCode);
+    _$hash = $jc(_$hash, minimumRate.hashCode);
+    _$hash = $jc(_$hash, kmForMinimumRate.hashCode);
+    _$hash = $jc(_$hash, perKmRate.hashCode);
+    _$hash = $jc(_$hash, capacity.hashCode);
+    _$hash = $jc(_$hash, name.hashCode);
+    _$hash = $jc(_$hash, companyModel.hashCode);
+    _$hash = $jc(_$hash, color.hashCode);
     _$hash = $jf(_$hash);
     return _$hash;
   }
@@ -45,7 +82,14 @@ class _$CabCreate extends CabCreate {
   String toString() {
     return (newBuiltValueToStringHelper(r'CabCreate')
           ..add('vehicleType', vehicleType)
-          ..add('vehicleNumber', vehicleNumber))
+          ..add('vehicleNumber', vehicleNumber)
+          ..add('minimumRate', minimumRate)
+          ..add('kmForMinimumRate', kmForMinimumRate)
+          ..add('perKmRate', perKmRate)
+          ..add('capacity', capacity)
+          ..add('name', name)
+          ..add('companyModel', companyModel)
+          ..add('color', color))
         .toString();
   }
 }
@@ -62,6 +106,35 @@ class CabCreateBuilder implements Builder<CabCreate, CabCreateBuilder> {
   set vehicleNumber(String? vehicleNumber) =>
       _$this._vehicleNumber = vehicleNumber;
 
+  num? _minimumRate;
+  num? get minimumRate => _$this._minimumRate;
+  set minimumRate(num? minimumRate) => _$this._minimumRate = minimumRate;
+
+  num? _kmForMinimumRate;
+  num? get kmForMinimumRate => _$this._kmForMinimumRate;
+  set kmForMinimumRate(num? kmForMinimumRate) =>
+      _$this._kmForMinimumRate = kmForMinimumRate;
+
+  num? _perKmRate;
+  num? get perKmRate => _$this._perKmRate;
+  set perKmRate(num? perKmRate) => _$this._perKmRate = perKmRate;
+
+  int? _capacity;
+  int? get capacity => _$this._capacity;
+  set capacity(int? capacity) => _$this._capacity = capacity;
+
+  String? _name;
+  String? get name => _$this._name;
+  set name(String? name) => _$this._name = name;
+
+  String? _companyModel;
+  String? get companyModel => _$this._companyModel;
+  set companyModel(String? companyModel) => _$this._companyModel = companyModel;
+
+  String? _color;
+  String? get color => _$this._color;
+  set color(String? color) => _$this._color = color;
+
   CabCreateBuilder() {
     CabCreate._defaults(this);
   }
@@ -71,6 +144,13 @@ class CabCreateBuilder implements Builder<CabCreate, CabCreateBuilder> {
     if ($v != null) {
       _vehicleType = $v.vehicleType;
       _vehicleNumber = $v.vehicleNumber;
+      _minimumRate = $v.minimumRate;
+      _kmForMinimumRate = $v.kmForMinimumRate;
+      _perKmRate = $v.perKmRate;
+      _capacity = $v.capacity;
+      _name = $v.name;
+      _companyModel = $v.companyModel;
+      _color = $v.color;
       _$v = null;
     }
     return this;
@@ -96,6 +176,20 @@ class CabCreateBuilder implements Builder<CabCreate, CabCreateBuilder> {
               vehicleType, r'CabCreate', 'vehicleType'),
           vehicleNumber: BuiltValueNullFieldError.checkNotNull(
               vehicleNumber, r'CabCreate', 'vehicleNumber'),
+          minimumRate: BuiltValueNullFieldError.checkNotNull(
+              minimumRate, r'CabCreate', 'minimumRate'),
+          kmForMinimumRate: BuiltValueNullFieldError.checkNotNull(
+              kmForMinimumRate, r'CabCreate', 'kmForMinimumRate'),
+          perKmRate: BuiltValueNullFieldError.checkNotNull(
+              perKmRate, r'CabCreate', 'perKmRate'),
+          capacity: BuiltValueNullFieldError.checkNotNull(
+              capacity, r'CabCreate', 'capacity'),
+          name:
+              BuiltValueNullFieldError.checkNotNull(name, r'CabCreate', 'name'),
+          companyModel: BuiltValueNullFieldError.checkNotNull(
+              companyModel, r'CabCreate', 'companyModel'),
+          color: BuiltValueNullFieldError.checkNotNull(
+              color, r'CabCreate', 'color'),
         );
     replace(_$result);
     return _$result;
--- dart_sdk/lib/src/model/cab_public.g.dart
+++ dart_sdk/lib/src/model/cab_public.g.dart
@@ -15,6 +15,20 @@ class _$CabPublic extends CabPublic {
   final String vehicleType;
   @override
   final String vehicleNumber;
+  @override
+  final num minimumRate;
+  @override
+  final num kmForMinimumRate;
+  @override
+  final num perKmRate;
+  @override
+  final int capacity;
+  @override
+  final String name;
+  @override
+  final String companyModel;
+  @override
+  final String color;
 
   factory _$CabPublic([void Function(CabPublicBuilder)? updates]) =>
       (CabPublicBuilder()..update(updates))._build();
@@ -23,7 +37,14 @@ class _$CabPublic extends CabPublic {
       {required this.id,
       required this.providerId,
       required this.vehicleType,
-      required this.vehicleNumber})
+      required this.vehicleNumber,
+      required this.minimumRate,
+      required this.kmForMinimumRate,
+      required this.perKmRate,
+      required this.capacity,
+      required this.name,
+      required this.companyModel,
+      required this.color})
       : super._();
   @override
   CabPublic rebuild(void Function(CabPublicBuilder) updates) =>
@@ -39,7 +60,14 @@ class _$CabPublic extends CabPublic {
         id == other.id &&
         providerId == other.providerId &&
         vehicleType == other.vehicleType &&
-        vehicleNumber == other.vehicleNumber;
+        vehicleNumber == other.vehicleNumber &&
+        minimumRate == other.minimumRate &&
+        kmForMinimumRate == other.kmForMinimumRate &&
+        perKmRate == other.perKmRate &&
+        capacity == other.capacity &&
+        name == other.name &&
+        companyModel == other.companyModel &&
+        color == other.color;
   }
 
   @override
@@ -49,6 +77,13 @@ class _$CabPublic extends CabPublic {
     _$hash = $jc(_$hash, providerId.hashCode);
     _$hash = $jc(_$hash, vehicleType.hashCode);
     _$hash = $jc(_$hash, vehicleNumber.hashCode);
+    _$hash = $jc(_$hash, minimumRate.hashCode);
+    _$hash = $jc(_$hash, kmForMinimumRate.hashCode);
+    _$hash = $jc(_$hash, perKmRate.hashCode);
+    _$hash = $jc(_$hash, capacity.hashCode);
+    _$hash = $jc(_$hash, name.hashCode);
+    _$hash = $jc(_$hash, companyModel.hashCode);
+    _$hash = $jc(_$hash, color.hashCode);
     _$hash = $jf(_$hash);
     return _$hash;
   }
@@ -59,7 +94,14 @@ class _$CabPublic extends CabPublic {
           ..add('id', id)
           ..add('providerId', providerId)
           ..add('vehicleType', vehicleType)
-          ..add('vehicleNumber', vehicleNumber))
+          ..add('vehicleNumber', vehicleNumber)
+          ..add('minimumRate', minimumRate)
+          ..add('kmForMinimumRate', kmForMinimumRate)
+          ..add('perKmRate', perKmRate)
+          ..add('capacity', capacity)
+          ..add('name', name)
+          ..add('companyModel', companyModel)
+          ..add('color', color))
         .toString();
   }
 }
@@ -84,6 +126,35 @@ class CabPublicBuilder implements Builder<CabPublic, CabPublicBuilder> {
   set vehicleNumber(String? vehicleNumber) =>
       _$this._vehicleNumber = vehicleNumber;
 
+  num? _minimumRate;
+  num? get minimumRate => _$this._minimumRate;
+  set minimumRate(num? minimumRate) => _$this._minimumRate = minimumRate;
+
+  num? _kmForMinimumRate;
+  num? get kmForMinimumRate => _$this._kmForMinimumRate;
+  set kmForMinimumRate(num? kmForMinimumRate) =>
+      _$this._kmForMinimumRate = kmForMinimumRate;
+
+  num? _perKmRate;
+  num? get perKmRate => _$this._perKmRate;
+  set perKmRate(num? perKmRate) => _$this._perKmRate = perKmRate;
+
+  int? _capacity;
+  int? get capacity => _$this._capacity;
+  set capacity(int? capacity) => _$this._capacity = capacity;
+
+  String? _name;
+  String? get name => _$this._name;
+  set name(String? name) => _$this._name = name;
+
+  String? _companyModel;
+  String? get companyModel => _$this._companyModel;
+  set companyModel(String? companyModel) => _$this._companyModel = companyModel;
+
+  String? _color;
+  String? get color => _$this._color;
+  set color(String? color) => _$this._color = color;
+
   CabPublicBuilder() {
     CabPublic._defaults(this);
   }
@@ -95,6 +166,13 @@ class CabPublicBuilder implements Builder<CabPublic, CabPublicBuilder> {
       _providerId = $v.providerId;
       _vehicleType = $v.vehicleType;
       _vehicleNumber = $v.vehicleNumber;
+      _minimumRate = $v.minimumRate;
+      _kmForMinimumRate = $v.kmForMinimumRate;
+      _perKmRate = $v.perKmRate;
+      _capacity = $v.capacity;
+      _name = $v.name;
+      _companyModel = $v.companyModel;
+      _color = $v.color;
       _$v = null;
     }
     return this;
@@ -123,6 +201,20 @@ class CabPublicBuilder implements Builder<CabPublic, CabPublicBuilder> {
               vehicleType, r'CabPublic', 'vehicleType'),
           vehicleNumber: BuiltValueNullFieldError.checkNotNull(
               vehicleNumber, r'CabPublic', 'vehicleNumber'),
+          minimumRate: BuiltValueNullFieldError.checkNotNull(
+              minimumRate, r'CabPublic', 'minimumRate'),
+          kmForMinimumRate: BuiltValueNullFieldError.checkNotNull(
+              kmForMinimumRate, r'CabPublic', 'kmForMinimumRate'),
+          perKmRate: BuiltValueNullFieldError.checkNotNull(
+              perKmRate, r'CabPublic', 'perKmRate'),
+          capacity: BuiltValueNullFieldError.checkNotNull(
+              capacity, r'CabPublic', 'capacity'),
+          name:
+              BuiltValueNullFieldError.checkNotNull(name, r'CabPublic', 'name'),
+          companyModel: BuiltValueNullFieldError.checkNotNull(
+              companyModel, r'CabPublic', 'companyModel'),
+          color: BuiltValueNullFieldError.checkNotNull(
+              color, r'CabPublic', 'color'),
         );
     replace(_$result);
     return _$result;
